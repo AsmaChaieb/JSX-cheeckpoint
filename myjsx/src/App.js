d0a33c941131b181ca9f060dfa35d7cbb1b1ab0a
asma chaieb <asma@gmail.com>
2022-02-06 12:30:50 +0100

    first commit

---
+++ myjsx/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import imageInSrc from './imageInSrc.jpg'
+
+function App() {
+  return (
+    <div className="App">
+
+<div style ={{ border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 className="title red">Chaieb Asma </h1>
+
+<br/ >
+
+<img src={imageInSrc} alt ="myImage" />
+<br/ >
+
+<img src="imageInPublic.jpg" alt=""/>
+
+</div>     
+    </div>
+  );
+}
+
+export default App;

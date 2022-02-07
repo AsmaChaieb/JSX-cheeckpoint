e03f9488ca65f63e6829b5dc0398f0d99ad54d3a
asma chaieb <asma@gmail.com>
2022-02-07 09:28:08 +0100

    second commit

--- myjsx/src/App.js
+++ myjsx/src/App.js
@@ -4,7 +4,7 @@ import imageInSrc from './imageInSrc.jpg'
 
 function App() {
   return (
-    <div className="App">
+<div className="App">
 
 <div style ={{ border:"solid 1px black",maxWidth:"100vw"}}>
 
@@ -17,8 +17,14 @@ function App() {
 
 <img src="imageInPublic.jpg" alt=""/>
 
-</div>     
-    </div>
+<br/>  
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+</div> 
+</div>  
   );
 }
 
